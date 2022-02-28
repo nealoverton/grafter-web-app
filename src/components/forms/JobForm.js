435b9f01910b0325a140fd70e06312f0aaa95c66
Jordan Richmond <richmond42@ymail.com>
2022-02-28 17:58:28 +0000

    complete

--- src/components/forms/JobForm.js
+++ src/components/forms/JobForm.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { useState, useEffect, useRef } from 'react';
 
 function JobForm(props) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -20,17 +27,37 @@ function JobForm(props) {
 
   return (
     <form className="job-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add a Job"
-        value={input}
-        name="text"
-        className="job-input"
-        onChange={handleChange}
-      />
-      <button type="submit" className="job-button">
-        Add Job
-      </button>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update Your Item"
+            value={input}
+            name="text"
+            className="job-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button type="submit" className="job-button edit">
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a Job"
+            value={input}
+            name="text"
+            className="job-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button type="submit" className="job-button">
+            Add Job
+          </button>
+        </>
+      )}
     </form>
   );
 }
