4123466d069ee8d0fa06b93256fddd96b9cf6905
Jordan Richmond <richmond42@ymail.com>
2022-02-28 17:58:53 +0000

    complete1

--- src/pages/JobPage.js
+++ src/pages/JobPage.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
+import JobForm from '../components/forms/JobForm';
 
-function JobPage({ jobs, completeJob, removeJob }) {
+function JobPage({ jobs, completeJob, removeJob, updateJob }) {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   });
 
+  const submitUpdate = (value) => {
+    updateJob(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <JobForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
   return jobs.map((job, index) => (
+    // eslint-disable-next-line react/no-array-index-key
     <div className={job.isComplete ? 'job-row complete' : 'job-row'} key={index}>
       <div key={job.id} onClick={() => completeJob(job.id)} aria-hidden="true">
         {job.text}
