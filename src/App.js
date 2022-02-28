f69b15af6ba998d968d33d779f887335f56263f9
nealoverton <nealjoverton@gmail.com>
2022-02-28 16:01:15 +0200

    display full job description with editable fields

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import JobsList from './pages/JobsListPage';
 import Nav from './components/layout/Navbar';
+import Job from './pages/Job';
 import { AuthProvider } from './contexts/AuthContext';
 import MaterialsList from './pages/MaterialsList';
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/calendar" element={<Calendar />} />
           <Route path="/jobs" element={<JobsList />} />
+          <Route path="/job" element={<Job />} />
           <Route path="/materials" element={<MaterialsList />} />
         </Routes>
       </AuthProvider>
