8c4ca506dca1ee15025090eb1938758c86be3274
Jordan Richmond <richmond42@ymail.com>
2022-02-28 13:22:07 +0000

    console working 2

--- src/pages/JobsListPage.js
+++ src/pages/JobsListPage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import JobForm from '../components/forms/JobForm';
 import './JobsListPage.css';
 
 function JobsList() {
+  const [jobs, setJobs] = useState([]);
+
+  const addJob = (job) => {
+    if (!job.text || /^\s*$/.test(job.text)) {
+      return;
+    }
+
+    const newJobs = [job, ...jobs];
+
+    setJobs(newJobs);
+    console.log(...jobs);
+  };
+
   return (
     <div className="jobs-list">
-      <JobForm />
+      <h1>What jobs do you have today?</h1>
+      <JobForm onSubmit={addJob} />
     </div>
   );
 }
