0eea61e7008b9cf588051bead5e2732ee1397193
nealoverton <nealjoverton@gmail.com>
2022-03-02 18:32:49 +0200

    click attachment to toggle fullscreen view

--- src/pages/Attachment.js
+++ src/pages/Attachment.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimesCircle } from 'react-icons/fa';
 import './Attachment.css';
 
 export const Attachment = function ({ attachment, deleteAttachment, index }) {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
   return (
-    <li key={attachment.url} className={isFullscreen ? 'fullscreen' : 'Attachment__container'}>
+    <li key={attachment.id} className={isFullscreen ? 'fullscreen' : 'Attachment__container'}>
       <button
         type="button"
         onClick={() => setIsFullscreen(!isFullscreen)}
@@ -17,7 +17,10 @@ export const Attachment = function ({ attachment, deleteAttachment, index }) {
           alt="Attached job info"
           className={isFullscreen ? 'fullscreen' : 'Attachment__img'}
         />
-        <FaTimes className="Attachment__delete-icon" onClick={() => deleteAttachment(index)} />
+        <FaTimesCircle
+          className="Attachment__delete-icon"
+          onClick={() => deleteAttachment(index)}
+        />
       </button>
     </li>
   );
