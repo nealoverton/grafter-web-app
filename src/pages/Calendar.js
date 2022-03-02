b11bcf2b34f095f3ca8b1334953ff63090addb7e
Hockless <jameshockless97@gmail.com>
2022-03-02 11:32:41 +0000

    more information showing on events

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -5,27 +5,45 @@ import databaseService from '../services/firestore';
 
 export const reactCalendar = function () {
   const [job, setJob] = useState([]);
+  const [info, setInfo] = useState([]);
 
   useEffect(() => {
     databaseService.getJobs('EBRklWxRHARgMY3PADB7omUiLuC2').then((jobsFromFirestore) => {
       console.log(jobsFromFirestore, '<<<<');
+
       if (jobsFromFirestore.length > 0) {
         const newJob = [
           {
-            title: `${jobsFromFirestore['0'].name}`,
+            title: `${jobsFromFirestore['0'].name} ${jobsFromFirestore['1'].firstAddressLine} ${jobsFromFirestore['1'].postcode} `,
+
             start: `${jobsFromFirestore['0'].jobStartDate}`,
             end: `${jobsFromFirestore['0'].jobEndDate}`
           }
         ];
+        const newInfo = [
+          {
+            Address: `${jobsFromFirestore['1'].firstAddressLine}`,
+            Postcode: `${jobsFromFirestore['1'].postcode}`
+          }
+        ];
         setJob(newJob);
+        setInfo(newInfo);
       }
     });
   }, []);
 
+  console.log(info);
   return (
     <div>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" weekends events={job} />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends
+        events={job}
+        eventClick
+      />
     </div>
   );
 };
+
 export default reactCalendar;
