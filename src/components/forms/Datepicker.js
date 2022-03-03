ede7e766b03b0beba765b98581da4d5e744f0593
nealoverton <nealjoverton@gmail.com>
2022-03-03 10:55:27 +0200

    set job page dates with date picker

---
+++ src/components/forms/Datepicker.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { FaTimesCircle } from 'react-icons/fa';
+import Calendar from 'react-calendar';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import 'react-calendar/dist/Calendar.css';
+import './Datepicker.css';
+
+dayjs.extend(customParseFormat);
+
+const DatePicker = function ({
+  startDate = dayjs(),
+
+  endDate = dayjs(),
+  handleDateSelection,
+  setDatePickerIsOpen
+}) {
+  const [value, setValue] = useState([
+    dayjs(startDate, 'DD/MM/YYYY'),
+    dayjs(endDate, 'DD/MM/YYYY')
+  ]);
+
+  const handleChange = (dates) => {
+    console.log(dates);
+    console.log(dates[0].toLocaleDateString());
+    setValue(dates);
+    handleDateSelection([dates[0].toLocaleDateString(), dates[1].toLocaleDateString()]);
+  };
+
+  return (
+    <div className="DatePicker__container">
+      <div className="DatePicker__header">
+        <p className="DatePicker__header__text">{`${startDate} - ${endDate}`}</p>
+        <FaTimesCircle
+          className="DatePicker__header__close-icon"
+          onClick={() => setDatePickerIsOpen(false)}
+        />
+      </div>
+      <Calendar onChange={handleChange} initialValue={value} selectRange />
+    </div>
+  );
+};
+
+export default DatePicker;
